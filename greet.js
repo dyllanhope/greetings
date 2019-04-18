3d14b4ca8ccba3e17bb2121849cc2f9f157ae99d
Dyllan Hope <dyllanjhope@gmail.com>
2019-04-18 15:52:01 +0200

    first commit

---
+++ greet.js
@@ -0,0 +1,7 @@
+var greetBtn = document.querySelector(".greetBtn");
+var greetFieldText = document.querySelector(".greetFieldtext");
+var greetElement = document.querySelector(".name");
+
+greetBtn.addEventListener('click', function(){
+    greetElement.innerHTML = "Hello, " + greetFieldText.value.trim();
+})
